47cced649e6b4b8550cc5a908e0fd416604e61d5
PranshuDixitDev <137303794+PranshuDixitDev@users.noreply.github.com>
2024-09-13 19:40:49 +0530

    allowed hosts updated

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -56,7 +56,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gujjumasala.in', 'api.gujjumasala.in', 'localhost', '127.0.0.1']
 
 
 # Application definition
